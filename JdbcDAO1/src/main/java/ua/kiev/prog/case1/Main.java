043b3016d6ad3d0903695752b6b9986d8a727a0d
OlexandrBorovik <100314860+OlexandrBorovik@users.noreply.github.com>
2022-07-15 23:13:55 +0300

    Add files via upload

---
+++ JdbcDAO1/src/main/java/ua/kiev/prog/case1/Main.java
@@ -0,0 +1,61 @@
+package ua.kiev.prog.case1;
+
+import ua.kiev.prog.shared.Client;
+import ua.kiev.prog.shared.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+// DB > JDBC > DAO > App
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try (Connection conn = ConnectionFactory.getConnection();
+             Scanner sc = new Scanner(System.in))
+        {
+            ClientDAO dao = new ClientDAOImpl(conn);
+            dao.createTable();
+
+            while (true) {
+                System.out.println("1: add client");
+                System.out.println("2: view clients");
+                System.out.println("3: view some colums");
+                System.out.println("4: view count");
+                System.out.print("-> ");
+
+                String s = sc.nextLine();
+                switch (s) {
+                    case "1":
+                        System.out.print("Enter client name: ");
+                        String name = sc.nextLine();
+                        System.out.print("Enter client age: ");
+                        String sAge = sc.nextLine();
+                        int age = Integer.parseInt(sAge);
+
+                        dao.addClient(name, age);
+                        break;
+                    case "2":
+                        List<Client> list = dao.getAll();
+                        for (Client client : list) {
+                            System.out.println(client);
+                        }
+                        break;
+                    case "3":
+                    List<Client> listAge = dao.getAge();
+                    for (Client client : listAge) {
+
+                        System.out.println(client);
+                    }
+                    break;
+                    case "4":
+                        System.out.println("Count: " + dao.count());
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+    }
+}
